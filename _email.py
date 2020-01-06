0da260c97c475313f31550ca7efb3c3750e0cf9d
youngjay90 <3254048915@qq.com>
2020-01-06 09:39:25 +0200

    feat: Add Cc mail option

--- _email.py
+++ _email.py
@@ -14,20 +14,16 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# If modifying these scopes, delete the file token.pickle.
-
 
 class Test:
+
     def __init__(self):
         SCOPES = ['https://www.googleapis.com/auth/gmail.send']
         self.file_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop\\data_extract')
-
-        """Shows basic usage of the Gmail API.
-        Lists the user's Gmail labels.
-        """
         parser = RawConfigParser()
         parser.read("db-creds.cfg")
-        self.recipients = (parser.get('emails', "emails").strip())
+        self.cc = parser.get('Cc', "Cc").strip()
+        self.recipients = parser.get('emails', "emails").strip()
         self.sender = parser.get('sender', "sender").strip()
         creds = None
         # The file token.pickle stores the user's access and refresh tokens, and is
@@ -37,7 +33,7 @@ class Test:
             with open('token.pickle', 'rb') as token:
                 print('here')
                 creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
+        # If there are no (valid) credentials available, lets the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
@@ -51,15 +47,15 @@ class Test:
 
         self.service = build('gmail', 'v1', credentials=creds)
 
-    def create_message_with_attachment(self, sender, to, subject, message_text):
+    def create_message_with_attachment(self, sender, to, cc, subject, message_text):
         message = MIMEMultipart()
         message['to'] = to
+        message['Cc'] = cc
         message['from'] = sender
         message['subject'] = subject
 
         msg = MIMEText(message_text)
         message.attach(msg)
-        print(message)
         for file_name in os.listdir(self.file_path):
             with open(f'{self.file_path}\\{file_name}', "rb") as attachment:
                 part = MIMEBase("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet")
@@ -73,17 +69,18 @@ class Test:
         return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
     def send_message(self):
-        # //Sends an email message.
-        # # Arguments:
-        # # service: an authorized Gmail API service instance.
-        # # user_id: User's email address. To indicate the authenticated user, the special value "me" can be used.
-        # # message: Message to be sent.
+        """Sends an email message.
+        Arguments:
+        service: an authorized Gmail API service instance.
+        user_id: User's email address. To indicate the authenticated user, the special value "me" can be used.
+        message: Message to be sent."""
         try:
             message = (self.service.users().messages().send(
                 userId='me',
                 body=self.create_message_with_attachment(
                     self.sender,
                     self.recipients,
+                    self.cc,
                     'Invoiced Devices Report',
                     'Please see attachments'
                 )).execute())
